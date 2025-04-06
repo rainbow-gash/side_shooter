d7f02593d081007c78e23ca1388dc33659611dc6
rainbow-gash <206418800+rainbow-gash@users.noreply.github.com>
2025-04-06 22:34:06 +0100

    Added several enemies with semi-random movement

---
+++ enemy.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, game_instance):
+		super().__init__()
+		self.screen = game_instance.screen
+		self.screen_rect = self.screen.get_rect()
+		self.frames = [
+			pygame.image.load('enemy.png'),
+			pygame.image.load('enemy_damaged.png')
+			]
+		self.image = self.frames[0]
+		self.rect = self.image.get_rect()
+		self.rect.midright = self.screen_rect.midright
+		self.x_speed = 6
+		self.y_speed = 2 + random.randint(-1, 1)
+		self.movement_direction = 1 # 1 for downwards, -1 for upwards
+		self.hit_points = 3
+
+	def update(self):
+		self.rect.x -= self.x_speed
+		self.rect.y += self.y_speed * self.movement_direction
+		if self.rect.bottom >= self.screen_rect.bottom:
+			self.movement_direction = -1
+		if self.rect.top <= self.screen_rect.top:
+			self.movement_direction = 1
--- side_shooter.py
+++ side_shooter.py
@@ -4,6 +4,7 @@ import random
 
 from ship import Ship
 from star import Star
+from enemy import Enemy
 
 class Game:
 	def __init__(self):
@@ -154,32 +155,6 @@ class Bullet(pygame.sprite.Sprite):
 
 	def update(self):
 		self.rect.x += 20
-
-
-class Enemy(pygame.sprite.Sprite):
-	def __init__(self, game_instance):
-		super().__init__()
-		self.screen = game_instance.screen
-		self.screen_rect = self.screen.get_rect()
-		self.frames = [
-			pygame.image.load('enemy.png'),
-			pygame.image.load('enemy_damaged.png')
-			]
-		self.image = self.frames[0]
-		self.rect = self.image.get_rect()
-		self.rect.topright = self.screen_rect.topright
-		self.x_speed = 6
-		self.y_speed = 2 + random.randint(-1, 1)
-		self.movement_direction = 1 # 1 for downwards, -1 for upwards
-		self.hit_points = 3
-
-	def update(self):
-		self.rect.x -= self.x_speed
-		self.rect.y += self.y_speed * self.movement_direction
-		if self.rect.bottom >= self.screen_rect.bottom:
-			self.movement_direction = -1
-		if self.rect.top <= self.screen_rect.top:
-			self.movement_direction = 1
 
 game = Game()
 game.run_game()
